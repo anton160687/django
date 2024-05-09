2fafc19dcd8f62d4ab315ee93f1c49d9a7600676
Антон <104146518+anton160687@users.noreply.github.com>
2024-05-10 01:50:01 +0300

    first commit2

--- djangomagazin/settings.py
+++ djangomagazin/settings.py
@@ -24,7 +24,7 @@ STATIC_ROOT = BASE_DIR / 'static/'
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a18m1zr@f52($gtw+iqqih(o3yg=k%k^2r5_!mg8$lc5wd4xi6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
